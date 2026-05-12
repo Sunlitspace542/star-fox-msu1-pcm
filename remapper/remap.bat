46dd9189a98b5520d0dee81b2c2a89f0423e5b1c
SunlitSpace542 <35207254+Sunlitspace542@users.noreply.github.com>
2026-05-12 02:09:54 -0400

    finalize rom prefix

--- remapper/remap.bat
+++ remapper/remap.bat
@@ -1,3 +1,3 @@
-python remap_pcm_tracks.py -c remap.csv -f input --source-prefix sfx_msu1 --dest-prefix SF -o output
+python remap_pcm_tracks.py -c remap.csv -f input --source-prefix sfx_msu1 --dest-prefix sf_msu1 -o output
 pause
 exit
